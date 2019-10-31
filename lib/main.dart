fd9e98fa3f87c21f1d8a903fdc6074ec7468e58a
Beloin <beloin.rodrigues@gmail.com>
2019-10-31 01:10:55 -0300

    new file:   .gitignore 	new file:   .metadata 	new file:   README.md 	new file:   android/app/build.gradle 	new file:   android/app/src/debug/AndroidManifest.xml 	new file:   android/app/src/main/AndroidManifest.xml 	new file:   android/app/src/main/kotlin/com/example/lista_tarefas/MainActivity.kt 	new file:   android/app/src/main/res/drawable/launch_background.xml 	new file:   android/app/src/main/res/mipmap-hdpi/ic_launcher.png 	new file:   android/app/src/main/res/mipmap-mdpi/ic_launcher.png 	new file:   android/app/src/main/res/mipmap-xhdpi/ic_launcher.png 	new file:   android/app/src/main/res/mipmap-xxhdpi/ic_launcher.png 	new file:   android/app/src/main/res/mipmap-xxxhdpi/ic_launcher.png 	new file:   android/app/src/main/res/values/styles.xml 	new file:   android/app/src/profile/AndroidManifest.xml 	new file:   android/build.gradle 	new file:   android/gradle.properties 	new file:   android/gradle/wrapper/gradle-wrapper.properties 	new file:   android/settings.gradle 	new file:   ios/Flutter/AppFrameworkInfo.plist 	new file:   ios/Flutter/Debug.xcconfig 	new file:   ios/Flutter/Release.xcconfig 	new file:   ios/Runner.xcodeproj/project.pbxproj 	new file:   ios/Runner.xcodeproj/project.xcworkspace/contents.xcworkspacedata 	new file:   ios/Runner.xcodeproj/xcshareddata/xcschemes/Runner.xcscheme 	new file:   ios/Runner.xcworkspace/contents.xcworkspacedata 	new file:   ios/Runner/AppDelegate.swift 	new file:   ios/Runner/Assets.xcassets/AppIcon.appiconset/Contents.json 	new file:   ios/Runner/Assets.xcassets/AppIcon.appiconset/Icon-App-1024x1024@1x.png 	new file:   ios/Runner/Assets.xcassets/AppIcon.appiconset/Icon-App-20x20@1x.png 	new file:   ios/Runner/Assets.xcassets/AppIcon.appiconset/Icon-App-20x20@2x.png 	new file:   ios/Runner/Assets.xcassets/AppIcon.appiconset/Icon-App-20x20@3x.png 	new file:   ios/Runner/Assets.xcassets/AppIcon.appiconset/Icon-App-29x29@1x.png 	new file:   ios/Runner/Assets.xcassets/AppIcon.appiconset/Icon-App-29x29@2x.png 	new file:   ios/Runner/Assets.xcassets/AppIcon.appiconset/Icon-App-29x29@3x.png 	new file:   ios/Runner/Assets.xcassets/AppIcon.appiconset/Icon-App-40x40@1x.png 	new file:   ios/Runner/Assets.xcassets/AppIcon.appiconset/Icon-App-40x40@2x.png 	new file:   ios/Runner/Assets.xcassets/AppIcon.appiconset/Icon-App-40x40@3x.png 	new file:   ios/Runner/Assets.xcassets/AppIcon.appiconset/Icon-App-60x60@2x.png 	new file:   ios/Runner/Assets.xcassets/AppIcon.appiconset/Icon-App-60x60@3x.png 	new file:   ios/Runner/Assets.xcassets/AppIcon.appiconset/Icon-App-76x76@1x.png 	new file:   ios/Runner/Assets.xcassets/AppIcon.appiconset/Icon-App-76x76@2x.png 	new file:   ios/Runner/Assets.xcassets/AppIcon.appiconset/Icon-App-83.5x83.5@2x.png 	new file:   ios/Runner/Assets.xcassets/LaunchImage.imageset/Contents.json 	new file:   ios/Runner/Assets.xcassets/LaunchImage.imageset/LaunchImage.png 	new file:   ios/Runner/Assets.xcassets/LaunchImage.imageset/LaunchImage@2x.png 	new file:   ios/Runner/Assets.xcassets/LaunchImage.imageset/LaunchImage@3x.png 	new file:   ios/Runner/Assets.xcassets/LaunchImage.imageset/README.md 	new file:   ios/Runner/Base.lproj/LaunchScreen.storyboard 	new file:   ios/Runner/Base.lproj/Main.storyboard 	new file:   ios/Runner/Info.plist 	new file:   ios/Runner/Runner-Bridging-Header.h 	new file:   lib/main.dart 	new file:   lista-tarefas 	new file:   pubspec.lock 	new file:   pubspec.yaml

---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: Home(),
+  ));
+}
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          "Lista de Tarefas",
+          style: TextStyle(color: Colors.white),
+        ),
+        backgroundColor: Colors.blueGrey,
+      ),
+    );
+  }
+}
